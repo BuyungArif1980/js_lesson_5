a2268235b04ef7278804e091181ed93c654bd4e3
yungbow <buyungarif.budpar@gmail.com>
2020-04-21 22:26:56 +0700

    Lesson 7. Paket (2)

--- data/dogData.js
+++ data/dogData.js
@@ -1,7 +1,19 @@
-// Ubah jalur relatif "./dog"
+// Import readline-sync
+import readlineSync from "readline-sync";
+
 import Dog from "../class/dog";
 
 const dog1 = new Dog("Leo", 4, "Chihuahua");
-const dog2 = new Dog("Ben", 2, "Poodle");
+
+// Tulis ulang dengan menggunakan readlineSync.question
+const name = readlineSync.question("Ketik nama Anda: ");
+
+// Tulis ulang dengan menggunakan readlineSync.questionInt
+const age = readlineSync.questionInt("Ketik usia Anda: ");
+
+// Tulis ulang dengan menggunakan readlineSync.question
+const breed = readlineSync.question("Ketik ras Anda: ");
+
+const dog2 = new Dog(name, age, breed);
 
 export { dog1, dog2 };
